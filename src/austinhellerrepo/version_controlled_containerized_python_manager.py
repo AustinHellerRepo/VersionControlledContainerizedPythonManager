f03b2870df5e28cc41c1ea3742f681ad1d923527
austin <galaky2005@gmail.com>
2021-11-10 21:51:58 -0500

    working version; added timeout to run; added tests;

--- src/austinhellerrepo/version_controlled_containerized_python_manager.py
+++ src/austinhellerrepo/version_controlled_containerized_python_manager.py
@@ -1,4 +1,103 @@
+from austin_heller_repo.docker_manager import DockerManager
+from austin_heller_repo.git_manager import GitManager, GitLocalRepositoryInstance
+from austin_heller_repo.threading import TimeoutThread
+from typing import List, Tuple, Dict
+
+
+class DockerContainerInstanceTimeoutException(Exception):
+
+	def __init__(self, *args: object):
+		super().__init__(*args)
+
+
+class VersionControlledContainerizedPythonInstance():
+
+	def __init__(self):
+		raise NotImplementedError()
+
+	def get_output(self) -> str:
+		raise NotImplementedError()
+
+	def wait(self):
+		raise NotImplementedError()
 
 
 class VersionControlledContainerizedPythonManager():
-	pass
+
+	def __init__(self, *, git_manager: GitManager):
+
+		self.__git_manager = git_manager
+
+	def run_python_script(self, *, git_repo_clone_url: str, script_file_path: str, script_arguments: List[str], timeout_seconds: float) -> str:
+
+		if not self.__git_manager.is_repository_cloned_locally(
+			git_url=git_repo_clone_url
+		):
+			git_local_repository_instance = self.__git_manager.clone(
+				git_url=git_repo_clone_url
+			)
+		else:
+			git_local_repository_instance = self.__git_manager.get_existing_local_repository_instance_from_url(
+				git_url=git_repo_clone_url
+			)
+			if git_local_repository_instance.is_remote_commit_different(
+				git_url=git_repo_clone_url
+			):
+				# TODO perform lock on git repo directory
+				git_local_repository_instance = self.__git_manager.clone(
+					git_url=git_repo_clone_url
+				)
+
+		docker_manager = DockerManager(
+			dockerfile_directory_path=git_local_repository_instance.get_directory_path()
+		)
+
+		git_project_name = GitManager.get_project_name_from_git_url(
+			git_url=git_repo_clone_url
+		)
+
+		docker_container_instance = docker_manager.start(
+			name=f"vccpm_{git_project_name.lower()}"
+		)
+
+		concat_script_arguments = ""
+		for script_argument_index, script_argument in enumerate(script_arguments):
+			if script_argument_index != 0:
+				concat_script_arguments += " "
+			concat_script_arguments += f"{script_argument}"
+
+		output = None  # type: bytes
+
+		def timeout_thread_method():
+			nonlocal output
+			nonlocal docker_container_instance
+			nonlocal script_arguments
+			nonlocal script_file_path
+			nonlocal concat_script_arguments
+
+			if len(script_arguments) == 0:
+				docker_container_instance.execute_command(
+					command=f"python {script_file_path}"
+				)
+			else:
+				docker_container_instance.execute_command(
+					command=f"python {script_file_path} {concat_script_arguments}"
+				)
+
+			docker_container_instance.wait()
+
+			output = docker_container_instance.get_stdout()
+
+		timeout_thread = TimeoutThread(timeout_thread_method, timeout_seconds)
+		timeout_thread.start()
+
+		is_successful = timeout_thread.try_wait()
+
+		docker_container_instance.stop()
+		docker_container_instance.remove()
+		docker_manager.dispose()
+
+		if not is_successful:
+			raise DockerContainerInstanceTimeoutException()
+
+		return output.decode()
---
+++ test/version_controlled_containerized_python_manager_test.py
@@ -0,0 +1,98 @@
+import unittest
+from src.austinhellerrepo.version_controlled_containerized_python_manager import VersionControlledContainerizedPythonManager, VersionControlledContainerizedPythonInstance, DockerContainerInstanceTimeoutException
+from austin_heller_repo.git_manager import GitManager
+import tempfile
+import docker
+
+
+class VersionControlledContainerizedPythonManagerTest(unittest.TestCase):
+
+	def setUp(self) -> None:
+
+		docker_client = docker.from_env()
+
+		image_names = [
+			"vccpm_testdockertimedelay"
+		]
+
+		for image_name in image_names:
+
+			try:
+				docker_client.containers.get(f"{image_name}").kill()
+			except Exception as ex:
+				pass
+
+			try:
+				docker_client.containers.get(f"{image_name}").remove()
+			except Exception as ex:
+				pass
+
+			try:
+				docker_client.images.remove(
+					image=f"{image_name}"
+				)
+			except Exception as ex:
+				pass
+
+		docker_client.close()
+
+	def test_initialize(self):
+
+		temp_directory = tempfile.TemporaryDirectory()
+
+		git_manager = GitManager(
+			git_directory_path=temp_directory.name
+		)
+
+		vccpm = VersionControlledContainerizedPythonManager(
+			git_manager=git_manager
+		)
+
+		self.assertIsNotNone(vccpm)
+
+		temp_directory.cleanup()
+
+	def test_run_time_delay_script_timeout_failed(self):
+
+		temp_directory = tempfile.TemporaryDirectory()
+
+		git_manager = GitManager(
+			git_directory_path=temp_directory.name
+		)
+
+		vccpm = VersionControlledContainerizedPythonManager(
+			git_manager=git_manager
+		)
+
+		with self.assertRaises(DockerContainerInstanceTimeoutException):
+			output = vccpm.run_python_script(
+				git_repo_clone_url="https://github.com/AustinHellerRepo/TestDockerTimeDelay.git",
+				script_file_path="start.py",
+				script_arguments=[],
+				timeout_seconds=5
+			)
+
+		temp_directory.cleanup()
+
+	def test_run_time_delay_script(self):
+
+		temp_directory = tempfile.TemporaryDirectory()
+
+		git_manager = GitManager(
+			git_directory_path=temp_directory.name
+		)
+
+		vccpm = VersionControlledContainerizedPythonManager(
+			git_manager=git_manager
+		)
+
+		output = vccpm.run_python_script(
+			git_repo_clone_url="https://github.com/AustinHellerRepo/TestDockerTimeDelay.git",
+			script_file_path="start.py",
+			script_arguments=[],
+			timeout_seconds=20
+		)
+
+		self.assertEqual('{ "data": [ ], "exception": null }\n', output)
+
+		temp_directory.cleanup()
